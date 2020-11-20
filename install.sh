57e352fcd007accf3ccb7506470554e432368aa2
RaspberryRBX <61115942+RaspberryRBX@users.noreply.github.com>
2020-11-20 21:35:37 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+          
+echo Upgrading System
+sudo apt update
+sudo apt upgrade
+echo Install Starting Please Wait
+          
+cd /home/pi
+          
+mkdir /home/pi/.local/share/Spotify
+          
+sudo mv /home/pi/Spotify/Spotify.desktop /usr/share/applications
+          
+sudo mv /home/pi/Spotify/icons/spotify-icon.png /usr/share/icons
+          
+sudo mv /home/pi/Spotify/spotify.sh /home/pi/.local/share/Spotify
+          
+cd /home/pi/.local/share/Spotify
+          
+sudo chmod +x spotify.sh
+          
+echo Install Done Running Spotify
+          
+./spotify.sh
+          
+cd
+          
+clear
+          
+echo Please Join The Discord https://discord.io/RaspberryRBX
+          
+echo Please Subscribe To RaspberryRBX On Youtube
+          
+echo This Script Was Created By RaspberryRBX And Phonix From Pilabs Discord
+echo Thx For My Discord Helping Me With These Stuff
+echo Hope U Enjoy This Webapp And Theres More Icons In Icons Folder
